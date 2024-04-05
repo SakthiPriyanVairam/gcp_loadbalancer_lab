a3e7e68886550da86115fc380728dd5486aced12
SakthiPriyanVairam <165206847+SakthiPriyanVairam@users.noreply.github.com>
2024-04-05 21:01:04 +0530

    Update gsp313.sh

--- gsp313.sh
+++ gsp313.sh
@@ -20,6 +20,7 @@ BOLD=`tput bold`
 RESET=`tput sgr0`
 #----------------------------------------------------start--------------------------------------------------#
 
+
 echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
 gcloud compute instances create $INSTANCE \
